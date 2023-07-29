252d4555bf9b67f3fb3a1a5884a2754eafd32029
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-29 15:56:29 +0700

    FE-v1.1.4.1 Added Landing Page

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,23 +2,31 @@ import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { data } = await axios.get(
+  //       `${process.env.REACT_APP_API_BASE_URL}/greetings`
+  //     );
+  //     setMessage(data?.message || "");
+  //   })();
+  // }, []);
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       {message}
+  //     </header>
+  //   </div>
+  // );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {message}
+        <LandingPage/>
       </header>
     </div>
   );
