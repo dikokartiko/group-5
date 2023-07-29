885e8b6a3312ce6d3e69c16b9a687f341974ed57
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-29 16:27:22 +0700

    FE-v4.1.1.2 Added Modal from chakra components

--- projects/client/src/components/modal/TabModal.js
+++ projects/client/src/components/modal/TabModal.js
@@ -1,6 +1,29 @@
-const TabModal = () => {
+import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Tab, TabList, TabPanels, Tabs } from '@chakra-ui/react';
+import React from 'react'
+
+const TabModal = (props) => {
   return (
-    <div>TabModal</div>
+    <Modal isOpen={props.isOpen} onClose={() => {
+      // modalOnClose();
+      // loginTab();
+      props.onClose();
+    }}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{props.title}</ModalHeader>
+        <ModalCloseButton />
+        <Tabs isFitted variant='enclosed'>
+          <TabList>
+            <Tab bg="lightgreen" borderTopLeftRadius={15} /*onClick={() => loginTab()}*/>Login</Tab>
+            <Tab bg="pink" borderTopRightRadius={15} /*onClick={/*() => registerTab()}*/>Register</Tab>
+          </TabList>
+          <TabPanels>
+            {/* <LoginForm />
+            <RegisterForm /> */}
+          </TabPanels>
+        </Tabs>
+      </ModalContent>
+    </Modal>
   )
 }
 
