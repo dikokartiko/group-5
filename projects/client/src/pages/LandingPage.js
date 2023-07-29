372c6dcb3d8253b2ed514f8baee9f89e6caff4f2
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-29 16:00:42 +0700

    FE-v2.1.1.1 Added login & register button

---
+++ projects/client/src/pages/LandingPage.js
@@ -0,0 +1,26 @@
+import { Box, Button, ButtonGroup } from '@chakra-ui/react'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <Box
+        bgColor="grey"
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        width='50%'
+        py={12}
+        // bgImage="url('https://bit.ly/2Z4KKcF')"
+        bgPosition='center'
+        bgRepeat='no-repeat'
+        mb={2}
+    >
+    <ButtonGroup gap='4'>
+        <Button colorScheme='red'>Login</Button>
+        <Button colorScheme='linkedin'>Register</Button>
+    </ButtonGroup>
+    </Box>
+  )
+}
+
+export default LandingPage
