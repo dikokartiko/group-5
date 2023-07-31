5c4c3dc4bb96da315bfc37b743fda4eea7ca6659
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-31 20:27:01 +0700

    FE-HOME-v5 Changed bgColor

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import { Box, VStack } from '@chakra-ui/react'
 import React from 'react'
 import LoginForm from '../components/form/LoginForm'
-import Hero from '../components/section/hero'
+import Hero from '../components/section/Hero'
 
 const LandingPage = () => {
   return (
@@ -11,7 +11,7 @@ const LandingPage = () => {
       height="calc(100vh)"
       width='100%'
     >
-      <Box flex={2} bgColor="grey"><Hero/></Box>
+      <Box flex={2} bgColor="lightgrey"><Hero/></Box>
       <VStack flex={1} bgColor={"blackAlpha.100"} justifyContent={"center"}>
         <LoginForm/>
       </VStack>
