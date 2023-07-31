f8fb2c968e13b9e3d621b9bc69efc3e9d7e9854d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-31 16:24:38 +0700

    FE-HOME-v4 Integrated Hero section

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,18 +1,18 @@
-import { Box, Input, VStack } from '@chakra-ui/react'
+import { Box, VStack } from '@chakra-ui/react'
 import React from 'react'
 import LoginForm from '../components/form/LoginForm'
+import Hero from '../components/section/hero'
 
 const LandingPage = () => {
   return (
     <Box
       display='flex'
-      // alignItems='center'
       justifyContent={"center"}
       height="calc(100vh)"
       width='100%'
     >
-      <Box flex={2} bgColor="grey">Welcoming message</Box>
-      <VStack flex={1} bgColor={"blackAlpha.100"} justifyContent={"center"} alignItems={"center"}>
+      <Box flex={2} bgColor="grey"><Hero/></Box>
+      <VStack flex={1} bgColor={"blackAlpha.100"} justifyContent={"center"}>
         <LoginForm/>
       </VStack>
     </Box>
