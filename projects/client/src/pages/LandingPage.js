fcbaff2e70957d3300e80eb37852fd1bfee1e57e
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-07-31 14:49:50 +0700

    FE-HOME-v3 Clean code

--- projects/client/src/pages/LandingPage.js
+++ projects/client/src/pages/LandingPage.js
@@ -1,26 +1,20 @@
-import { Box, Button, ButtonGroup, useDisclosure } from '@chakra-ui/react'
+import { Box, Input, VStack } from '@chakra-ui/react'
 import React from 'react'
-import TabModal from '../components/modal/TabModal'
-import RegularModal from '../components/modal/RegularModal'
 import LoginForm from '../components/form/LoginForm'
 
 const LandingPage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-
     <Box
-      bgColor="grey"
       display='flex'
       // alignItems='center'
-      // justifyContent='center'
+      justifyContent={"center"}
       height="calc(100vh)"
       width='100%'
-      // py={12}
-      // bgImage="url('https://bit.ly/2Z4KKcF')"
-      // bgPosition='center'
-      // bgRepeat='no-repeat'
     >
-      <LoginForm></LoginForm>
+      <Box flex={2} bgColor="grey">Welcoming message</Box>
+      <VStack flex={1} bgColor={"blackAlpha.100"} justifyContent={"center"} alignItems={"center"}>
+        <LoginForm/>
+      </VStack>
     </Box>
   )
 }
