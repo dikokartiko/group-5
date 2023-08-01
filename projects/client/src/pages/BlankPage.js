f4b9d916a4a7e1ed26b053512e1460fb68c5185b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 06:27:04 +0700

    FE-COMP-5-v3 Added components

--- projects/client/src/pages/BlankPage.js
+++ projects/client/src/pages/BlankPage.js
@@ -1,16 +1,30 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Divider, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const BlankPage = () => {
+const BlankPage = (props) => {
   return (
     <Box
+      bgColor="lightgrey"
       display='flex'
       justifyContent={"center"}
       height="calc(100vh)"
       width='100%'
     >
-      <Box>
-        <Text as="b" fontSize={"6xl"}>TUPO</Text>
+      <Box
+        alignItems="center"
+        bgColor="whiteAlpha.500"
+        display="flex"
+        flexDirection="column"
+        justifyItems="start"
+        width="50%"
+      >
+        <Box width="100%" textAlign="center">
+          <Text textAlign="center" as="b" fontSize="6xl" marginBottom="4">TUPO</Text>
+        </Box>
+        <Divider/>
+        <Box height="100%" width="100%" display="flex" justifyContent="center" alignItems="center">
+          {props.children}
+        </Box>
       </Box>
     </Box>
   )
