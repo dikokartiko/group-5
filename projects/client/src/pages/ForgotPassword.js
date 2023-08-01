c1e6ab0cbfdfd46e06d210b4067534b12912f41f
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 06:25:21 +0700

    FE-FORG-v3 Added components and axios

--- projects/client/src/pages/ForgotPassword.js
+++ projects/client/src/pages/ForgotPassword.js
@@ -1,9 +1,48 @@
 import React from 'react'
 import BlankPage from './BlankPage'
+import InputWithError from '../components/input/InputWithError'
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+import axios from 'axios';
+import { Box, Button, Input } from '@chakra-ui/react';
 
 const ForgotPassword = () => {
+  const forgotPasswordSchema = useFormik({
+      initialValues: {
+          email: "",
+      },
+      validationSchema: Yup.object({
+          email: Yup.string()
+            .required("Email tidak boleh kosong!")
+            .email("Format email tidak benar!")
+      }),
+      onSubmit: async values => {
+          // alert(JSON.stringify(values, null, 2));
+
+          // const axios = require("axios");
+          await axios.post("https://minpro-blog.purwadhikabootcamp.com/api/auth/login", {
+              email: values.email,
+          }).then(resp => {
+              alert(`[resp.data]: ${resp.data}`);
+          }).catch(error => {
+              alert(`[error.response.data.err] ${error.response.data.err}`);
+          });
+          alert("Done");
+      }
+  });
   return (
-    <BlankPage></BlankPage>
+    <BlankPage>
+      <Box bgColor={"white"} borderRadius={"md"} boxShadow='md' height="30%" padding={5} textColor={"black"} width={"50%"} >
+        <form onSubmit={forgotPasswordSchema.handleSubmit}>
+          <InputWithError errors={forgotPasswordSchema.errors.email} touched={forgotPasswordSchema.touched.email}>
+            <Input type="text" name="email" placeholder='Email' bgColor="white" borderColor={"grey"} color={"black"} value={forgotPasswordSchema.values.email} onChange={forgotPasswordSchema.handleChange}/>
+          </InputWithError>
+          <Box display={"flex"}>
+            <Button type="submit" colorScheme={"green"} flex={1} marginX="5">Kirim</Button>
+          </Box>
+        </form>
+      </Box>
+    </BlankPage>
   )
 }
 
