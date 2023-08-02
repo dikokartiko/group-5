0dd6a097ec358296273839426bbbddbbff9b9a6f
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 11:47:34 +0700

    FE-PREP-v5 Added Success Page to route

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import ForgotPassword from "./pages/ForgotPassword";
+import SuccessPage from "./pages/guest/SuccessPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/forgotPassword" element={<ForgotPassword/>} />
+        <Route path="/forgotPassword/emailSent" element={<SuccessPage/>} />
       </Routes>
     </Router>
   );
