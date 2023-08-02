f73698152e1f2b4bb97737cc4c139d2af380c5e5
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 07:50:44 +0700

    FE-COMP-4-v2 Added preset Box component as main component

--- projects/client/src/components/card/FormCard.js
+++ projects/client/src/components/card/FormCard.js
@@ -1,8 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
-const FormCard = () => {
+const FormCard = (props) => {
   return (
-    <div>FormCard</div>
+    <Box bgColor={"white"} borderRadius={"md"} boxShadow='md' minHeight="30%" padding={5} textAlign={"center"} textColor={"black"} width={"50%"}>
+        {props.children}
+    </Box>
   )
 }
 
