3ee6eb1b750e0f3dc3b88cd4a992473f5a968a73
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 15:32:24 +0700

    FE-COMP-3-v4 Added 'isConfirm' for different placeholder

--- projects/client/src/components/input/InputPassword.js
+++ projects/client/src/components/input/InputPassword.js
@@ -7,7 +7,7 @@ const InputPassword = (props) => {
         name={props.name}
         pr='4.5rem'
         type={props.show ? 'text' : 'password'}
-        placeholder='Kata sandi'
+        placeholder={props.isConfirm? 'Konfirmasi kata sandi' : 'Kata sandi'}
         bgColor="white" 
         borderColor={"grey"} 
         color={"black"}
