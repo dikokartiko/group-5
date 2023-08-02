0ae4be4e3d1bb6093b178962191e0a6f4c2ee63c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 11:44:58 +0700

    FE-COMP-4-v3 Added display, alignItems, & justifyContent

--- projects/client/src/components/card/FormCard.js
+++ projects/client/src/components/card/FormCard.js
@@ -3,8 +3,22 @@ import React from 'react'
 
 const FormCard = (props) => {
   return (
-    <Box bgColor={"white"} borderRadius={"md"} boxShadow='md' minHeight="30%" padding={5} textAlign={"center"} textColor={"black"} width={"50%"}>
-        {props.children}
+    <Box 
+      bgColor={"white"} 
+      borderRadius={"md"} 
+      boxShadow='md' 
+      flexDirection="column"
+      minHeight="30%" 
+      padding={5} 
+      textAlign={"center"} 
+      textColor={"black"} 
+      width={"50%"} 
+      
+      display="flex" 
+      alignItems={"stretch"}
+      justifyContent={"center"}
+    >
+      {props.children}
     </Box>
   )
 }
