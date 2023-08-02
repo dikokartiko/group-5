bcf150fba82bb17ec708849fb64300662bebe6f9
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-02 11:46:03 +0700

    FE-SUCC-v2 Added components

--- projects/client/src/pages/guest/SuccessPage.js
+++ projects/client/src/pages/guest/SuccessPage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import BlankPage from '../BlankPage'
+import FormCard from '../../components/card/FormCard'
+import { Box, Text } from '@chakra-ui/react'
+import { TbMailCheck } from 'react-icons/tb'
 
 const SuccessPage = () => {
   return (
-    <div>SuccessPage</div>
+    <BlankPage>
+        <FormCard>
+            <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                <TbMailCheck size={70}/>
+            </Box>
+            <Text as="b" fontSize="2xl">Permintaan sudah kami terima</Text>
+            <Text>Silahkan periksa email anda untuk instruksi berikutnya</Text>
+        </FormCard>
+    </BlankPage>
   )
 }
 
