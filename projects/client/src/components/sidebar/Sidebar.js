2e4125cdacb9ac2baca7ff6e30371b0c957edb7d
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 22:24:26 +0700

    FE-SIDE-v4 Added Navigation Item (NavItem)

--- projects/client/src/components/sidebar/Sidebar.js
+++ projects/client/src/components/sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import { Box, Divider, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
-import { TbCalculator } from 'react-icons/tb';
+import { TbBoxSeam, TbCalculator, TbCategory, TbReportAnalytics, TbUsers } from 'react-icons/tb';
 
 const Sidebar = () => {
   return (
@@ -16,6 +16,36 @@ const Sidebar = () => {
     //     </DrawerContent>
     // </Drawer>
     <Box height="90vh" width={["", "15%"]} boxShadow={'md'}>
+        <Box display={"flex"} alignItems={"center"} paddingX={5} paddingY={2} bgColor={"transparent"} borderBottom={"1px"} borderColor={"lightgrey"}>
+            <Box paddingRight={5}>
+                <TbCalculator size={30} />
+            </Box>
+            <Text fontSize={"xl"}>Cashier</Text>
+        </Box>
+        <Box display={"flex"} alignItems={"center"} paddingX={5} paddingY={2} bgColor={"transparent"} borderBottom={"1px"} borderColor={"lightgrey"}>
+            <Box paddingRight={5}>
+                <TbBoxSeam size={30} />
+            </Box>
+            <Text fontSize={"xl"}>Produk</Text>
+        </Box>
+        <Box display={"flex"} alignItems={"center"} paddingX={5} paddingY={2} bgColor={"transparent"} borderBottom={"1px"} borderColor={"lightgrey"}>
+            <Box paddingRight={5}>
+                <TbCategory size={30} />
+            </Box>
+            <Text fontSize={"xl"}>Kategori</Text>
+        </Box>
+        <Box display={"flex"} alignItems={"center"} paddingX={5} paddingY={2} bgColor={"transparent"} borderBottom={"1px"} borderColor={"lightgrey"}>
+            <Box paddingRight={5}>
+                <TbUsers size={30} />
+            </Box>
+            <Text fontSize={"xl"}>User Manajemen</Text>
+        </Box>
+        <Box display={"flex"} alignItems={"center"} paddingX={5} paddingY={2} bgColor={"transparent"} borderBottom={"1px"} borderColor={"lightgrey"}>
+            <Box paddingRight={5}>
+                <TbReportAnalytics size={30} />
+            </Box>
+            <Text fontSize={"xl"}>Laporan Penjualan</Text>
+        </Box> 
     </Box>
   )
 }
