080b9a43d20920871e4d03c302bb71598a91344a
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 14:19:39 +0700

    FE-DASH-v3 Added Box for page placement

--- projects/client/src/pages/user/Dashboard.js
+++ projects/client/src/pages/user/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
+import { Box } from '@chakra-ui/react'
 
 const Dashboard = () => {
   return (
     <>
       <Sidebar />
+      <Box display="flex" width="100%" bgColor={"lightgrey"}>
+        
+      </Box>
     </>
   )
 }
