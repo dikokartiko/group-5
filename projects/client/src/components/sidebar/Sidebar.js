d5f70f0fa7ef44b2bd0c2b05205bbe560cbe9350
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 22:07:20 +0700

    FE-SIDE-v3 Set Sidebar height to '90vh'

--- projects/client/src/components/sidebar/Sidebar.js
+++ projects/client/src/components/sidebar/Sidebar.js
@@ -1,19 +1,22 @@
-import { Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay } from '@chakra-ui/react'
+import { Box, Divider, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
+import { TbCalculator } from 'react-icons/tb';
 
 const Sidebar = () => {
   return (
-    <Drawer placement={"left"} /*onClose={onClose}*/ isOpen={true}>
-        <DrawerOverlay />
-        <DrawerContent>
-            <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
-            <DrawerBody>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </DrawerBody>
-        </DrawerContent>
-    </Drawer>
+    // <Drawer placement={"left"} /*onClose={onClose}*/ isOpen={true}>
+    //     <DrawerOverlay />
+    //     <DrawerContent>
+    //         <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+    //         <DrawerBody>
+    //             <p>Some contents...</p>
+    //             <p>Some contents...</p>
+    //             <p>Some contents...</p>
+    //         </DrawerBody>
+    //     </DrawerContent>
+    // </Drawer>
+    <Box height="90vh" width={["", "15%"]} boxShadow={'md'}>
+    </Box>
   )
 }
 
