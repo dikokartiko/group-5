73f931b4af9f1cead16f70a172454ec89569c606
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 11:15:41 +0700

    FE-MAIN-v3 Added Sidebar custom component

--- projects/client/src/pages/user/Main.js
+++ projects/client/src/pages/user/Main.js
@@ -1,11 +1,12 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import BlankPage from '../BlankPage'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const MainPage = () => {
   return (
     <BlankPage>
-
+        <Sidebar/>
     </BlankPage>
   )
 }
