b61a9e5cc9e602d01630ba13d5c2f7fd3e44fd51
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 14:34:01 +0700

    FE-DASH-v4 Added props.children

--- projects/client/src/pages/user/Dashboard.js
+++ projects/client/src/pages/user/Dashboard.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import { Box } from '@chakra-ui/react'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   return (
     <>
       <Sidebar />
-      <Box display="flex" width="100%" bgColor={"lightgrey"}>
-        
+      <Box display="flex" height="100vh" width="100%" bgColor={"lightgrey"}>
+        {props.children}
       </Box>
     </>
   )
