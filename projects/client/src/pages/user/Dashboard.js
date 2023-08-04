48f7ac00dc3054110523be9a8c4a17ae7833740a
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-04 14:58:18 +0700

    FE-DASH-v5 Added Header custom component

--- projects/client/src/pages/user/Dashboard.js
+++ projects/client/src/pages/user/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
 import { Box } from '@chakra-ui/react'
+import Header from '../../components/section/Header'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Dashboard = (props) => {
   return (
     <>
+      <Header />
       <Sidebar />
       <Box display="flex" height="100vh" width="100%" bgColor={"lightgrey"}>
         {props.children}
