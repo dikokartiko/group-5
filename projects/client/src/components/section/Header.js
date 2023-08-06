b25a8838561fd804c293ee8b2df66c254bc3a496
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-06 14:52:24 +0700

    FE-HEAD-v5 Added Avatar button

--- projects/client/src/components/section/Header.js
+++ projects/client/src/components/section/Header.js
@@ -1,10 +1,15 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Avatar, Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
     <Box boxShadow={'md'} height={"10vh"}>
         <Text marginX={5} textAlign="center" as="b" fontSize="6xl">TUPO</Text>
+        <Box borderRadius={15} boxShadow={"md"}>
+            <Avatar name="Dimas Ivon Anggitama" src="#"/>
+            <Text>Dimas Ivon Anggitama</Text>
+            <Text>Admin</Text>
+        </Box>
     </Box>
   )
 }
