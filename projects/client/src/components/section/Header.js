b2f882d88906743e770b93a323447e7509fbc4a6
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-06 16:25:29 +0700

    FE-HEAD-v6 Fixed components placement

--- projects/client/src/components/section/Header.js
+++ projects/client/src/components/section/Header.js
@@ -1,15 +1,17 @@
-import { Avatar, Box, Text } from '@chakra-ui/react'
+import { Avatar, Box, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <Box boxShadow={'md'} height={"10vh"}>
+    <Box display={"flex"} justifyContent={"space-between"} height={"10vh"} boxShadow={'md'} bgColor={"blackAlpha.50"}>
         <Text marginX={5} textAlign="center" as="b" fontSize="6xl">TUPO</Text>
-        <Box borderRadius={15} boxShadow={"md"}>
-            <Avatar name="Dimas Ivon Anggitama" src="#"/>
-            <Text>Dimas Ivon Anggitama</Text>
-            <Text>Admin</Text>
-        </Box>
+        <HStack margin={5} paddingX={3} borderRadius={15} boxShadow={"md"} bgColor={"white"}>
+            <Avatar name="Dimas Ivon Anggitama" src="#" marginRight={5}/>
+            <Box>
+                <Text>Dimas Ivon Anggitama</Text>
+                <Text>Admin</Text>
+            </Box>
+        </HStack>
     </Box>
   )
 }
