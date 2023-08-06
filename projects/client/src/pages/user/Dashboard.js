93c83f8c3b4c3063ae2d0ffd7a1b996294967590
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-06 09:38:42 +0700

    FE-DASH-v7 Renamed Sidebar component path file

--- projects/client/src/pages/user/Dashboard.js
+++ projects/client/src/pages/user/Dashboard.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
 import Header from '../../components/section/Header'
-import Sidebar from '../../components/sidebar/Sidebar'
+import Sidebar from '../../components/section/Sidebar'
 
 const Dashboard = (props) => {
   return (
-    <>
+    <Box height={"100vh"}>
       <Header />
       <Box display={"flex"} flexDirection={"row"}>
         <Sidebar />
-        <Box display="flex" height="100vh" width="100%" bgColor={"lightgrey"}>
+        <Box display={"flex"} width="100%" bgColor={"lightgrey"}>
           {props.children}
         </Box>
       </Box>
-    </>
+    </Box>
   )
 }
 
