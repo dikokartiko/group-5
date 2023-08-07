5fc3e36a3f0b77af35052fab983c389b66473dfe
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-07 20:23:05 +0700

    FE-CART-v8 Changed margin & padding value + fixed scrollable on Cart section only

--- projects/client/src/components/section/Cart.js
+++ projects/client/src/components/section/Cart.js
@@ -4,68 +4,74 @@ import { TbShoppingCartPlus } from 'react-icons/tb'
 
 const Cart = () => {
   return (
-    <Box width={["", "30%"]} padding={5} bgColor={"blackAlpha.50"}>
-        <Box width={"100%"} overflow={"auto"} >
+    <Box maxHeight={"90vh"} width={["", "30%"]} padding={5} bgColor={"blackAlpha.50"}  overflowY={"auto"}>
+        <Box width={"100%"}>
             <Image src="/decoration/Ripped_Papper_Edited.drawio.png" alt="Gambar" transform="scaleY(-1)" height={"2%"}/>
-            <Box height={"98%"} >
+            <Box>
                 <Box display={"flex"} flexDirection={'column'} justifyContent={"start"} alignItems={"center"} padding={5} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"} >
                     <Box>
                         <TbShoppingCartPlus size={70} />
                     </Box>
                     <Text as="b" fontSize="2xl">Keranjang</Text>
                 </Box>
-                <Box display={"flex"} flexDirection={'column'} padding={5} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                <Box display={"flex"} flexDirection={'column'} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>999x</Box>
+                        <Box maxWidth={"12%"}>999x</Box>
                         <Box maxWidth={"100%"}>Rp. 99.999.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>999x</Box>
+                        <Box maxWidth={"12%"}>999x</Box>
                         <Box maxWidth={"100%"}>Rp. 99.999.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} paddingX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>999x</Box>
+                        <Box maxWidth={"12%"}>999x</Box>
                         <Box maxWidth={"100%"}>Rp. 99.999.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>999x</Box>
+                        <Box maxWidth={"100%"}>Rp. 99.999.000,-</Box>
+                    </Box>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
+                        <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
-                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginTop={5} marginBottom={5}/>
-                    <Box display={"flex"} justifyContent={"space-between"}>
+                    <Box borderBottomColor={"gray.300"} borderBottomWidth={1} marginX={5}/>
+                    <Box display={"flex"} justifyContent={"space-between"} padding={5} _hover={{background: "#FEEBC8"}}>
                         <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
-                        <Box maxWidth={"11%"}>1x</Box>
+                        <Box maxWidth={"12%"}>1x</Box>
                         <Box maxWidth={"100%"}>Rp. 11.000,-</Box>
                     </Box>
                 </Box>
