b99a3f9196150c9c668e1faaf2c7543a2db5f9eb
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-07 13:54:11 +0700

    FE-CART-v3 Shaped Cart area to be like receipt paper

--- projects/client/src/components/section/Cart.js
+++ projects/client/src/components/section/Cart.js
@@ -1,10 +1,21 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { TbShoppingCartPlus } from 'react-icons/tb'
 
 const Cart = () => {
   return (
-    <Box display={"flex"} height={"90vh"} width={["", "25%"]}>
-        This is a test
+    <Box width={["", "25%"]} padding={5} bgColor={"blackAlpha.50"}>
+        <Box height="100%" width={"100%"} >
+            <Image src="/decoration/Ripped_Papper_Edited.drawio.png" alt="Gambar" transform="scaleY(-1)" height={"2%"}/>
+            <Box display={"flex"} flexDirection={'column'} justifyContent={"start"} alignItems={"center"} height="98%" padding={5} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
+                <Box>
+                    <TbShoppingCartPlus size={70} />
+                </Box>
+                <Text as="b" fontSize="2xl">Keranjang</Text>
+                <Box height="50%" width={"100%"} marginY={5} borderTopColor={"gray.300"} borderTopStyle={"dashed"} borderTopWidth={3} />
+            </Box>
+            <Image src="/decoration/Ripped_Papper_Edited.drawio.png" alt="Gambar" height={"2%"}/>
+        </Box>
     </Box>
   )
 }
