58c2b21760d48ffe6837452b94462ee2199fa6b8
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-07 21:05:33 +0700

    FE-POBU-v9 Changed popover placement to 'left-end'

--- projects/client/src/components/modal/PopoverButton.js
+++ projects/client/src/components/modal/PopoverButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const PopoverButton = (props) => {
   return (
-    <Popover placement='left' closeOnBlur={false}>
+    <Popover placement='left-end' closeOnBlur={false}>
         <PopoverTrigger>
             {props.children}
         </PopoverTrigger>
