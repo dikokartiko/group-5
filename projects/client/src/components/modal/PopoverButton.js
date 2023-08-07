62c4979214f83e81c19ef586946764b88071e9ba
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-07 21:06:04 +0700

    FE-POBU-v10 Deleted unusued imported components

--- projects/client/src/components/modal/PopoverButton.js
+++ projects/client/src/components/modal/PopoverButton.js
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger } from '@chakra-ui/react'
+import { Button, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger } from '@chakra-ui/react'
 import React from 'react'
 
 const PopoverButton = (props) => {
