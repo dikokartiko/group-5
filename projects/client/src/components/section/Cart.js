0833938df53cedb7d814ce0249306df887b87f9c
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-07 15:24:10 +0700

    FE-CART-v5 Fixed max width of item list

--- projects/client/src/components/section/Cart.js
+++ projects/client/src/components/section/Cart.js
@@ -4,11 +4,11 @@ import { TbShoppingCartPlus } from 'react-icons/tb'
 
 const Cart = () => {
   return (
-    <Box width={["", "25%"]} padding={5} bgColor={"blackAlpha.50"}>
-        <Box width={"100%"} overflow={"auto"}>
+    <Box width={["", "30%"]} padding={5} bgColor={"blackAlpha.50"}>
+        <Box width={"100%"} overflow={"auto"} >
             <Image src="/decoration/Ripped_Papper_Edited.drawio.png" alt="Gambar" transform="scaleY(-1)" height={"2%"}/>
-            <Box height={"98%"}>
-                <Box display={"flex"} flexDirection={'column'} justifyContent={"start"} alignItems={"center"} padding={5} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
+            <Box height={"98%"} >
+                <Box display={"flex"} flexDirection={'column'} justifyContent={"start"} alignItems={"center"} padding={5} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"} >
                     <Box>
                         <TbShoppingCartPlus size={70} />
                     </Box>
@@ -16,31 +16,10 @@ const Cart = () => {
                 </Box>
                 <Box display={"flex"} flexDirection={'column'} padding={5} borderBottomColor={"gray.300"} borderBottomStyle={"dashed"} borderBottomWidth={3} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
                     <Box display={"flex"} justifyContent={"space-between"}>
-                        <Box>Mie Iblis M Level 1</Box>
-                        <Box>1x</Box>
-                        <Box>Rp. 11.000,-</Box>
+                        <Box maxWidth={"50%"}>Mie Iblis M Level 1</Box>
+                        <Box maxWidth={"11%"}>999x</Box>
+                        <Box maxWidth={"100%"}>Rp. 99.999.000,-</Box>
                     </Box>
-                    <TableContainer>
-                        <Table variant='simple'>
-                            <Tbody>
-                                <Tr>
-                                    <Td>Mie Iblis M Level 1</Td>
-                                    <Td>1x</Td>
-                                    <Td>Rp. 11.000,-</Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Mie Iblis M Level 2</Td>
-                                    <Td>1x</Td>
-                                    <Td>Rp. 11.000,-</Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Mie Iblis M Level 3</Td>
-                                    <Td>1x</Td>
-                                    <Td>Rp. 11.000,-</Td>
-                                </Tr>
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
                 </Box>
                 <Box display={"flex"} flexDirection={'column'} justifyContent={"start"} alignItems={"center"} padding={5} boxShadow={"md"} bgColor={"white"} bgImage={"url('/decoration/A Box.drawio.png')"} bgRepeat={"repeat"}>
                     <Box>
