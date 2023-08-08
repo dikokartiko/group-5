e2c302472970295c5d73a13aa228e6380491ce62
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-08 19:17:27 +0700

    FE-PRCA-v4 Deleted Card description

--- projects/client/src/components/card/ProductCard.js
+++ projects/client/src/components/card/ProductCard.js
@@ -13,11 +13,6 @@ const ProductCard = () => {
         />
         <Stack mt='6' spacing='3'>
           <Heading size='md'>Living room Sofa</Heading>
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired
-            spaces, earthy toned spaces and for people who love a chic design with a
-            sprinkle of vintage design.
-          </Text>
           <Text color='blue.600' fontSize='2xl'>
             $450
           </Text>
