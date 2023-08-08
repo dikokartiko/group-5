686ea49d585f6fa3851921ee8fe93ecdc59a3068
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-08 19:15:26 +0700

    FE-PRCA-v3 Deleted Card Footer & Divider component

--- projects/client/src/components/card/ProductCard.js
+++ projects/client/src/components/card/ProductCard.js
@@ -4,11 +4,12 @@ import React from 'react'
 const ProductCard = () => {
   return (
     <Card maxW='sm'>
-      <CardBody>
+      <CardBody padding={0}>
         <Image
           src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
           alt='Green double couch with wooden legs'
-          borderRadius='lg'
+          borderTopRadius='lg'
+          borderBottomRadius='none'
         />
         <Stack mt='6' spacing='3'>
           <Heading size='md'>Living room Sofa</Heading>
@@ -22,17 +23,6 @@ const ProductCard = () => {
           </Text>
         </Stack>
       </CardBody>
-      <Divider />
-      <CardFooter>
-        <ButtonGroup spacing='2'>
-          <Button variant='solid' colorScheme='blue'>
-            Buy now
-          </Button>
-          <Button variant='ghost' colorScheme='blue'>
-            Add to cart
-          </Button>
-        </ButtonGroup>
-      </CardFooter>
     </Card>
   )
 }
