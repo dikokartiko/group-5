e5c8b0e76e29e70add3d8c21365d9ee04dbf6fe2
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-08 21:04:54 +0700

    FE-TEST-v2 Updated Test Page

--- projects/client/src/pages/TestPage.js
+++ projects/client/src/pages/TestPage.js
@@ -1,9 +1,13 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import ProductCard from '../components/card/ProductCard';
 
 const TestPage = () => {
   return (
-    <div>TestPage</div>
-  )
+    <Box display="flex" justifyContent="center" alignItems="center" height="100%" width="100%" bgColor={"grey"}>
+      <ProductCard></ProductCard>
+    </Box>
+  );
 }
 
 export default TestPage
