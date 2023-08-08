c26d5fbdfd69fa64a3ffd9f7dc2298266506c634
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-08 19:06:43 +0700

    FE-MAIN-v7 Added Cart + Simple Grid + Product Card component

--- projects/client/src/pages/user/MainPage.js
+++ projects/client/src/pages/user/MainPage.js
@@ -1,14 +1,22 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Grid, GridItem, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
 import Dashboard from './Dashboard'
+import Cart from '../../components/section/Cart'
+import ProductCard from '../../components/card/ProductCard'
 
 const MainPage = () => {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <>
-        <Dashboard>
-            This is Main Page
-        </Dashboard>
-    </>
+    <Dashboard>
+      <Box display={"flex"} width="100%" bgColor={"grey"}>
+        <SimpleGrid columns={[1, 2, 5]} spacing="1" justifyContent={"center"} margin={1}>
+          {data.map((item, index) => (
+            <ProductCard /*key={index} title={item}*/ />
+          ))}
+        </SimpleGrid>
+      </Box>
+      <Cart/>
+    </Dashboard>
   )
 }
 
